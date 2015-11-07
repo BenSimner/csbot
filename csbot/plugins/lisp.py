6db1d66e3244c9d47b1f4bcad38fecab98ee1441
Ben Simner <bs829@york.ac.uk>
2015-11-07 21:39:21 +0000

    Add basic lexer

--- csbot/plugins/lisp.py
+++ csbot/plugins/lisp.py
@@ -1,10 +1,13 @@
+from string import whitespace
 from enum import Enum
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 from abc import ABC, abstractmethod, abstractproperty
 
 from csbot.plugin import Plugin
 
 Token = namedtuple('Token', ['token_type', 'lexeme'])
+MATCHES = None
+'''String matching tree'''
 
 class TokenType(Enum):
     UNKNOWN = 0
@@ -17,7 +20,8 @@ class TokenType(Enum):
     # Scheme Symbols are the symbol \' (apostrophe) followed by a Name
     SYMBOL = 2
 
-    NUMBER = 3
+    # Scheme strings are strings of printable characters enclosed in ``"``
+    STRING = 3
 
     # L_PAREN, R_PAREN are opening and closing parenthesises, respectfully
     L_PAREN = 4
@@ -27,8 +31,18 @@ class TokenType(Enum):
     L_BRACKET = 6
     R_BRACKET = 7
 
-    # Scheme strings are strings of printable characters enclosed in ``"``
-    STRING = 8
+    NUMBER = 8
+    IMAGINARY = 9
+
+SYMBOL_LUT = {
+    '(': TokenType.L_PAREN,
+    ')': TokenType.R_PAREN,
+    '[': TokenType.L_BRACKET,
+    ']': TokenType.R_BRACKET
+}
+
+# Symbols do not require whitespace between them
+SYMBOLS = ('(', ')', '[', ']')
 
 class AST(ABC):
     '''The abstract-syntax-tree base for a scheme-like language
@@ -46,13 +60,111 @@ class AST(ABC):
         '''Returns an iterable of children :class:`AST`s associated with this class
         '''
 
-def lex(s):
-    '''Perform lexical analysis on the given string *s*
+def build_lexchar_dict(**words):
+    '''Given a map of strings->``TokenType`` *words* build a tree from a dict allowing easy checking of a word.'''
+    matches = LexCharDict()
 
-    If *s* is accepted, then this will return a list of *Token*s
+    for word,tok in words.items():
+        match = matches
+        for letter in word:
+            match = match[letter]
+        match._tok=tok # overwrite token type for this match
+
+    return matches
+
+class LexCharDict(defaultdict):
+    '''A ``defaultdict`` which maps single characters to other defaultdicts
+        this behaves as a tree, allowing lookup of tokentypes of lexeme's incrementally in a loop
+    
+    This class utilizes ``defaultdict`` as a FSA to accept regular languages and determine
+    the :class:``TokenType`` of the given input string.
+    '''
+
+    def __init__(self, tok=None):
+        super().__init__()
+        self._tok = tok
+        
+    def __missing__(self, k):
+        d = self.__class__(self._tok)
+
+        if self._tok in (None, '.', TokenType.NUMBER):
+            try:
+                int(k)
+                if d._tok in (None, '.'):
+                    d._tok = TokenType.NUMBER
+            except:
+                if k == 'i':
+                    d._tok = TokenType.IMAGINARY
+                elif k == '.':
+                    d._tok = '.'
+                else:
+                    d._tok = TokenType.NAME
+        elif self._tok == TokenType.IMAGINARY:
+            d._tok = TokenType.NAME
+
+        self[k] = d
+        return d
+
+class LexemeDict(defaultdict):
+    '''A ``defaultdict`` which maps lexeme's (strings) to their token types
+    '''
+    def __init__(self, matches):
+        super().__init__()
+        self._matches = matches
+
+    def check_type(self, tok):
+        if type(tok) == str:
+            raise ValueError('LexerError: Unexpected `{}`'.format(tok))
+
+    def __missing__(self, lex):
+        match = self._matches
+
+        for letter in lex:
+            match = match[letter]
+
+        self.check_type(match._tok)
+
+        self[lex] = match._tok
+        return self[lex]
+
+class ProgramDict(defaultdict):
+    '''A ``defaultdict`` which maps program strings to their tokens
+    '''
+
+    def __init__(self, symbol_list, lexeme_dict):
+        super().__init__()
+        self._symbols = symbol_list
+        self._dict = lexeme_dict
+
+    def __missing__(self, s):
+        lex_dict = self._dict
+        symbols = self._symbols
+
+        def _gen():
+            word = ''
+            for letter in s:
+                if letter in whitespace or letter in symbols:
+                    if word:
+                        tok_type = lex_dict[word]
+                        yield Token(tok_type, word)
+                    if letter in symbols:
+                        yield Token(lex_dict[letter], letter)
+                    word = ''
+                else:
+                    word += letter
+
+        self[s] = _gen()
+        return self[s]
+
+def lex(program_dict, s):
+    '''Perform lexical analysis on the given string *s* using a match dictionary *program_dict* 
+    (see :class:``ProgramDict``)
+
+    If *s* is accepted, then this will return a generator of *Token*s
     If *s* is not accepted, then this will raise `~ValueError`
     '''
-    raise ValueError
+
+    return program_dict[s]
 
 def parse(ts):
     '''Performs syntactical analysis on the given iterable of tokens *ts*
@@ -72,13 +184,24 @@ def eval(ast):
 
     raise ValueError
 
-class LispyEval(Plugin):
+class LispEval(Plugin):
     '''Performs evaluations on given strings interpreted as a scheme/lisp input
     '''
+
+    def _eval(self, s):
+        matches = build_lexchar_dict(**SYMBOL_LUT)
+        symbols = SYMBOLS
+
+        lexeme_dict = LexemeDict(matches)
+        program_dict = ProgramDict(symbols, lexeme_dict)
+        tk_gen = lex(program_dict, s)
+        
+        raise NotImplementedError
 
     @Plugin.command('eval')
     def do_eval(self, e):
         '''Evaluate the data in *e* as if it were a lisp string
         Sending the reply
         '''
-        e.reply(e["data"])
+
+        raise NotImplementedError
